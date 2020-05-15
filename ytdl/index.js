4ab2c4bd51326f383f277fbe7a0ad69e818de07a
RohitRajP <RohitRajP>
2020-05-15 18:04:57 +0530

    Still getting 404 (Rohit PC)

--- ytdl/index.js
+++ ytdl/index.js
@@ -2,17 +2,20 @@ const ytdl = require("ytdl-core");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    const vid = req.query && req.query.vid ? req.query.vid : null;
-    if (!vid) {
+    const vid = req.query.vid ? req.query.vid : null;
+    if (vid) {
+        const info = await ytdl.getInfo(vid);
+        context.res = {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: info,
+        };
+    } else {
         context.res = {
             status: 400,
             body: JSON.stringify("vid not found")
         };
     }
-    const info = await ytdl.getInfo(vid);
-    context.res = {
-        status: 200,
-        body: JSON.stringify(info),
-    };
-    context.done();
 };
